7b1fc703732b92878e8678cda08ea72a183e1caf
Sam Bender <rednebmas@gmail.com>
2015-12-23 10:58:44 -0800

    Initial Commit

---
+++ PitchEstimator/PitchEstimator.h
@@ -0,0 +1,19 @@
+//
+//  PitchEstimator.h
+//  PitchEstimator
+//
+//  Created by Sam Bender on 12/23/15.
+//  Copyright © 2015 Sam Bender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PitchEstimator.
+FOUNDATION_EXPORT double PitchEstimatorVersionNumber;
+
+//! Project version string for PitchEstimator.
+FOUNDATION_EXPORT const unsigned char PitchEstimatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PitchEstimator/PublicHeader.h>
+
+
